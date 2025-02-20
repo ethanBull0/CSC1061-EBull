11b376b8880e4e98f8ada39455000591814a3144
Ethan Bull <syntek108@gmail.com>
2025-02-20 09:50:13 -0700

    2/20/25

---
+++ Phone4U/src/FlipModel.java
@@ -0,0 +1,16 @@
+
+public class FlipModel implements Repairable {
+
+	@Override
+	public void howToRepair() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double costToRepair() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Phone4U/src/Repairable.java
@@ -0,0 +1,5 @@
+
+public interface Repairable {
+	void howToRepair();
+	double costToRepair();
+}

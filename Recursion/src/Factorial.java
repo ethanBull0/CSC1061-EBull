7e95097bd08f7bd806336425ce0a58ccc6c2baa2
Ethan Bull <syntek108@gmail.com>
2025-04-04 11:15:56 -0600

    4/4/25

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,9 +1,15 @@
 
 public class Factorial {
-
+	private static long counter = 0;
+	private static long[] termArray = new long[200];
+	
+	
 	public static void main(String[] args) {
-		System.out.println(factorial(15));
-		System.out.println(fibbonaci(50));
+		int fibNumber = 25;
+		System.out.println("Fibonacci sequence entry " + fibNumber + " is " + fibDynamic(fibNumber));
+		
+		
+		System.out.println("Call counter: " + counter);
 
 	}
 
@@ -14,13 +20,30 @@ public class Factorial {
 		return n * factorial(n - 1);
 	}
 	
-	public static int fibbonaci(long n) {
-		if (n == 0) {
-			return 0;
-		}
-		if (n == 1) {
-			return 1;
+	public static long fibbonaci(long n) {
+		counter++;
+		if (n <= 2) {
+			return n;
 		}
 		return  fibbonaci(n - 1) + fibbonaci(n - 2);
+	}
+	
+	public static long fibDynamic(int n) {
+		if (n < 2) {
+			return n;
+		}
+		if (termArray[n] > 0) {
+			return termArray[n];
+		}
+		counter++;
+		long ans = fibDynamic(n - 1) + fibDynamic(n - 2);
+		termArray[n] = ans;
+		return ans;
+	}
+	
+	public static long fibIterative(int n) {
+		if (n < 2) {
+			return n;
+		}
 	}
 }

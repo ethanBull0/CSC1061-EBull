6a387c1137e47c23d1b8695871589cc19f5c898e
Ethan Bull <syntek108@gmail.com>
2025-04-11 10:22:37 -0600

    4/11/25

---
+++ MyTreeMap/src/main/java/edu/frcc/csc1061jsp25/MyTreeMap/MyTreeMap.java
@@ -0,0 +1,220 @@
+package edu.frcc.csc1061jsp25.MyTreeMap;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class MyTreeMap<K, V> implements Map<K, V>, Iterable<V> {
+
+	private Node root = null;
+	private int size = 0;
+	
+	private class Node {
+		private K key;
+		private V value;
+		private Node left;
+		private Node right;
+		
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		/*Node current = root;
+		Comparable<K> k = (Comparable<K>) key;
+		while (current != null) {
+			if (current.left.equals(key)) {
+				return true;
+			}
+			current = current.left;
+		}
+		current = root;
+		while (current != null) {
+			if (current.right.equals(key)) {
+				return true;
+			}
+		}
+		return root.equals(key); */
+		if (get(key) != null) {
+			return true;
+		} 
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V get(Object key) {
+		Node current = root;
+		Comparable<K> k = (Comparable<K>) key;
+		
+		while(current != null) {
+			if (k.compareTo(current.key) < 0) {
+				current = current.left;
+			} else if (k.compareTo(current.key) > 0) {
+				current = current.right;
+			} else {
+				return current.value;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		if (root == null) {
+			Node newNode = new Node(key, value);
+			root = newNode;
+			size++;
+			return null;
+		}
+		
+		Node parent = null;
+		Node current = root;
+		Comparable<K> k = (Comparable<K>) key; //This is so we can call compareTo()
+		while (current != null) {
+			parent = current;
+			if (k.compareTo(current.key) < 0) {
+				current = current.left;
+			} else if (k.compareTo(current.key) > 0){
+				current = current.right;
+			} else {
+				V oldVal = current.value;
+				current.value = value;
+				return oldVal;
+			}
+		}
+		
+		Node newNode = new Node(key, value);
+		if (k.compareTo(parent.key) < 0) {
+			parent.left = newNode;
+		} else {
+			parent.right = newNode;
+		}
+		size++;
+		
+		return null;
+	}
+
+	//homework
+	@Override
+	public V remove(Object key) {
+		V value = get(key);
+		Node findNode = new Node((K)key, value);
+		RecursiveIterator ri = new RecursiveIterator();
+		while (ri.hasNext()) {
+			if (ri.next().equals(findNode)) {
+				
+			}
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<K> keySet() {
+		
+		return null;
+	}
+
+	@Override
+	public Collection<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<K> iterator() {
+		// TODO Auto-generated method stub
+		return new RecursiveIterator();
+	}
+	
+	private class RecursiveIterator implements Iterator<V> {
+		
+		private Queue<V> list = new ArrayDeque<>();
+		
+		public RecursiveIterator() {
+			preorder(root);
+		}
+		
+		private void preorder(Node node) {
+			if (node == null) {
+				return;
+			}
+			list.add(node.value);
+			preorder(node.left);
+			preorder(node.right);
+		}
+		
+		private void postorder(Node node) {
+			if (node == null) {
+				return;
+			}
+			list.add(node.value);
+			preorder(node.left);
+			preorder(node.right);
+		}
+		
+		private void inorder(Node node) {
+			if (node == null) {
+				return;
+			}
+			inorder(node.left);
+			list.add(node.value);
+			inorder(node.right);
+			
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return !list.isEmpty();
+		}
+
+		@Override
+		public V next() {
+			return list.remove(); //treat it like a queue
+		}
+		
+	}
+
+}
